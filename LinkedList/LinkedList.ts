98109557530bceaace653feb9a11ac3d5f28d427
ailei <jerry.ai@ucloud.cn>
2019-08-01 16:48:33 +0800

    stash

--- LinkedList/LinkedList.ts
+++ LinkedList/LinkedList.ts
@@ -1,3 +1,21 @@
 export function add(a:number, b:number) : number {
   return a + b;
 }
+
+class Node {
+  element: any;
+
+  next() {}
+}
+
+class LinkedList {
+  append() {}
+
+  removeAt() {}
+
+  insert() {}
+
+  indexOf() {}
+
+  toString() {}
+}
