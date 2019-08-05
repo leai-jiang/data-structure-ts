4226aa469160e558254867b866bc5a177bfc0eb6
ailei <jerry.ai@ucloud.cn>
2019-08-05 15:00:06 +0800

    stack

--- Stack/Stack.ts
+++ Stack/Stack.ts
@@ -3,15 +3,34 @@ export class Stack<T> {
 
   private arr: T[] = [];
 
-  push() {
-
+  // 入栈
+  push(element: T): T {
+    this.arr.push(element);
+    this.top++;
+    return element;
   }
-  
-  pop() {
 
+  // 出栈
+  pop(): T | null {
+    if (this.top > 0) {
+      return this.arr[--this.top];
+    }
+    return null;
   }
 
-  peek() {
+  peek(): T | null {
+    if (this.top > 0) {
+      return this.arr[this.top - 1];
+    }
+    return null;
+  }
 
+  clear() {
+    this.top = 0;
+    this.arr = [];
+  }
+
+  size(): number {
+    return this.top;
   }
 }
